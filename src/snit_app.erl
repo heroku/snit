7a7cc1adc0220255d8421873d910b99d5144085b
Fred Hebert <mononcqc@ferd.ca>
2015-05-14 16:23:14 -0400

    Only boot if the right cipher suites are supported
    Anything else is dead on arrival, although the configuration values can
    let people override things.

--- src/snit_app.erl
+++ src/snit_app.erl
@@ -1,8 +1,7 @@
 %%%-------------------------------------------------------------------
-%% @doc snit public API
+%% @doc snit application callback
 %% @end
 %%%-------------------------------------------------------------------
-
 -module(snit_app).
 
 -behaviour(application).
@@ -16,12 +15,22 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+    check_ciphers(),
     snit_sup:start_link().
 
-%%--------------------------------------------------------------------
 stop(_State) ->
     ok.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+check_ciphers() ->
+    {ok, Requirements} = application:get_env(snit, cipher_suites),
+    Ciphers = [Suite || {_Name, Suite} <- Requirements],
+    Supported = ssl:cipher_suites(erlang),
+    case [Cipher || Cipher <- Ciphers, not lists:member(Cipher, Supported)] of
+        [] ->
+            ok;
+        Unsupported ->
+            exit({unsupported_ciphers, Unsupported})
+    end.

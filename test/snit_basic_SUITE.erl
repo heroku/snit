f652cd2691be456a656195edb54307ffb49cc41f
Evan Vigil-McClanahan <evan@heroku.com>
2015-05-15 10:41:07 -0700

    add a connection test using in-memory certs

--- test/snit_basic_SUITE.erl
+++ test/snit_basic_SUITE.erl
@@ -8,7 +8,7 @@ groups() ->
     [].
 
 all() ->
-    [connect, connect_sni, proxy].
+    [connect, connect_sni, proxy, in_mem_connect].
 
 init_per_suite(Config) ->
     {ok, _} = application:ensure_all_started(snit),
@@ -41,6 +41,17 @@ init_per_testcase(connect_sni, Config) ->
                                       ?config(data_dir, Config) ++ "key.pem"}}
               ),
     Config;
+init_per_testcase(in_mem_connect, Config) ->
+    snit:start(connect_sni, 2, 8001, fun test_sni_fun_mem/1, snit_echo, []),
+    ets:new(test_tab, [public, named_table]),
+    {ok, CaCert0} = file:read_file(?config(data_dir, Config) ++ "cacerts.pem"),
+    [{_, CaCert}] = public_key:pem_decode(CaCert0),
+    {ok, Cert0} = file:read_file(?config(data_dir, Config) ++ "cert.pem"),
+    Cert = public_key:pem_decode(Cert0),
+    {ok, Key0} = file:read_file(?config(data_dir, Config) ++ "key.pem"),
+    Key = public_key:pem_decode(Key0),
+    ets:insert(test_tab, {"memhost", {CaCert, Cert, Key}}),
+    Config;
 init_per_testcase(proxy, Config) ->
     {ok, Listen} = gen_tcp:listen(0, [{active, false}]),
     {ok, {{0,0,0,0}, Port}} = inet:sockname(Listen),
@@ -61,6 +72,10 @@ end_per_testcase(connect, _Config) ->
     ets:delete(test_tab),
     ok;
 end_per_testcase(connect_sni, _Config) ->
+    snit:stop(connect_sni),
+    ets:delete(test_tab),
+    ok;
+end_per_testcase(in_mem_connect, _Config) ->
     snit:stop(connect_sni),
     ets:delete(test_tab),
     ok;
@@ -102,6 +117,22 @@ connect_sni(Config) ->
     end,
     Config.
 
+in_mem_connect(Config) ->
+    {ok, S} = ssl:connect("localhost", 8001,
+                          [{active, true},binary,
+                           {server_name_indication, "memhost"}]),
+    ssl:send(S, <<"first">>),
+    receive
+        {ssl, S, <<"first">>} ->
+            ok;
+        Else ->
+            lager:info("else ~p", [Else]),
+            error(unexpected_message)
+    after 500 ->
+            error(timeout)
+    end,
+    Config.
+
 proxy(Config) ->
     {ok, S} = ssl:connect("localhost", 8001,
                           [{active, true},binary,
@@ -122,6 +153,11 @@ test_sni_fun(SNIHostname) ->
     lager:debug("sni hostname: ~p", [SNIHostname]),
     [{_, {CaCertFile, CertFile, KeyFile}}] = ets:lookup(test_tab, SNIHostname),
     [{cacertfile, CaCertFile}, {certfile, CertFile}, {keyfile, KeyFile}].
+
+test_sni_fun_mem(SNIHostname) ->
+    lager:debug("sni hostname: ~p", [SNIHostname]),
+    [{_, {CaCert, Cert, Key}}] = ets:lookup(test_tab, SNIHostname),
+    [{cacert, CaCert}, {cert, Cert}, {key, Key}].
 
 listen(Listen) ->
     {ok, Sock} = gen_tcp:accept(Listen),
